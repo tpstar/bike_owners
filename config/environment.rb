1957ba2cb17867e8f224994ca807ffab38491db4
tpstar <hanlee12@gmail.com>
2017-06-24 21:13:22 -0500

    change to postgres for DATABASE_URL access

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,11 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 require_all 'app'
